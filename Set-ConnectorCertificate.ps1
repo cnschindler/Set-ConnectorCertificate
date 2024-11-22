e8eb8cf43accc77dd058ea680627852608caaeb7
Christian Schindler <cnschindler@gmx.net>
2024-11-22 09:29:59 +0100

    first commit

---
+++ Set-ConnectorCertificate.ps1
@@ -0,0 +1,25 @@
+﻿[cmdletbinding()]
+Param(
+    [Parameter(Manadatory=$true)]
+    $thumbprint,
+    [Parameter(Manadatory=$true)]
+    $Connector,
+    [Parameter(Manadatory=$true)]
+    [ValidateSet("Receive","Send")]
+    $ConnectorType
+)
+
+$TLSCert = Get-ExchangeCertificate -Thumbprint $thumbprint
+$TLSCertName = "<I>$($TLSCert.Issuer)<S>$($TLSCert.Subject)"
+
+if ($ConnectorType -eq "Receive")
+{
+    Set-ReceiveConnector -Identity $Connector -TlsCertificateName $null
+    Set-ReceiveConnector -Identity $Connector -TlsCertificateName $TLSCertName
+}
+
+if ($ConnectorType -eq "Send")
+{
+    Set-SendConnector -Identity $Connector -TlsCertificateName $null
+    Set-SendConnector -Identity $Connector -TlsCertificateName $TLSCertName
+}
