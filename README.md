09475653e9f47f6bc0e2e34c1fef437a7b502b71
Christian Schindler <cnschindler@gmx.net>
2024-11-22 09:43:42 +0100

    Error handling and README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Set-ConnectorCertificate
+
+Script to change the Certificate configured on the "TLSCertificateName" property on Receive/Send Connectors in Exchange OnPremises.
--- Set-ConnectorCertificate.ps1
+++ Set-ConnectorCertificate.ps1
@@ -14,12 +14,50 @@ $TLSCertName = "<I>$($TLSCert.Issuer)<S>$($TLSCert.Subject)"
 
 if ($ConnectorType -eq "Receive")
 {
-    Set-ReceiveConnector -Identity $Connector -TlsCertificateName $null
-    Set-ReceiveConnector -Identity $Connector -TlsCertificateName $TLSCertName
+    try
+    {
+        Set-ReceiveConnector -Identity $Connector -TlsCertificateName $null -ErrorAction Stop
+        Write-Host -ForegroundColor Green -Object "Successfully removed value from property 'TLSCertificateName' on $($ConnectorType)-Connector $($Connector)"
+    }
+
+    catch
+    {
+        Write-Host -ForegroundColor Red -Object "Error removing value from property 'TLSCertificateName' on $($ConnectorType)-Connector $($Connector). Error: $($_)"
+    }
+
+    try
+    {
+        Set-ReceiveConnector -Identity $Connector -TlsCertificateName $TLSCertName -ErrorAction Stop
+        Write-Host -ForegroundColor Green -Object "Successfully set the value of property 'TLSCertificateName' to $($TLSCertName) on $($ConnectorType)-Connector $($Connector)"
+    }
+
+    catch
+    {
+        Write-Host -ForegroundColor Red -Object "Error setting value $($TLSCertName) for property 'TLSCertificateName' on $($ConnectorType)-Connector $($Connector). Error: $($_)"        
+    }
 }
 
 if ($ConnectorType -eq "Send")
 {
-    Set-SendConnector -Identity $Connector -TlsCertificateName $null
-    Set-SendConnector -Identity $Connector -TlsCertificateName $TLSCertName
+    try
+    {
+        Set-SendConnector -Identity $Connector -TlsCertificateName $null -ErrorAction Stop
+        Write-Host -ForegroundColor Green -Object "Successfully removed value from property 'TLSCertificateName' on $($ConnectorType)-Connector $($Connector)"
+    }
+
+    catch
+    {
+        Write-Host -ForegroundColor Red -Object "Error removing value from property 'TLSCertificateName' on $($ConnectorType)-Connector $($Connector). Error: $($_)"
+    }
+
+    try
+    {
+        Set-SendConnector -Identity $Connector -TlsCertificateName $TLSCertName -ErrorAction Stop
+        Write-Host -ForegroundColor Green -Object "Successfully set the value of property 'TLSCertificateName' to $($TLSCertName) on $($ConnectorType)-Connector $($Connector)"
+    }
+
+    catch
+    {
+        Write-Host -ForegroundColor Red -Object "Error setting value $($TLSCertName) for property 'TLSCertificateName' on $($ConnectorType)-Connector $($Connector). Error: $($_)"        
+    }
 }
